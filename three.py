1ec24514919e3ea943aefc6b4437ac1121253969
ArunAnilal <arunjobs321@gmail.com>
2019-03-05 15:31:17 -0800

    the biggest in three

---
+++ three.py
@@ -0,0 +1,16 @@
+a=100
+b=89
+c=101
+if (a>b):
+    if(a>c):
+        print("the BIGGEST is",a)
+    else:
+        print("the BIGGEST is",c)
+else:
+    if(b>c):
+        print("the BIGGEST is",b)
+    else:
+        print("the BIGGEST is",c)   
+ 
+
+
