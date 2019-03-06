15174ad498f35cc52b004328d83f3ade036ac228
ArunAnilal <arunjobs321@gmail.com>
2019-03-06 12:46:13 -0800

    Creating a web page in python

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return "welcome to my home page"
+
+@app.route("/contact")
+def home():
+    return "welcome to your contact"
+
+if(__name__=="__main__"):
+    app.run(debug=True)
